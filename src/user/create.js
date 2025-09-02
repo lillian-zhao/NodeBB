ab4d243b5978155071b1e32ee8259bd9cfeaa20b
lillian <lillian.m.zhao@gmail.com>
2025-09-02 23:40:29 +0000

    changes to create.js for qlty smells

--- src/user/create.js
+++ src/user/create.js
@@ -11,7 +11,124 @@ const groups = require('../groups');
 const meta = require('../meta');
 const analytics = require('../analytics');
 
+//THE FOLLOWING HELPER FUNCTIONS FROM LINE 15 to 127 WERE WRITTEN WITH THE HELP OF CHATGPT
+/* ---------- HELPERS (top-level) ---------- */
+
+async function lock(value, error) {
+	const count = await db.incrObjectField('locks', value);
+	if (count > 1) {
+		throw new Error(error);
+	}
+}
+
+function buildUserData(data, timestamp) {
+	const baseData = {
+		username: data.username,
+		userslug: data.userslug,
+		joindate: timestamp,
+		lastonline: timestamp,
+		status: 'online',
+	};
+
+	const optionalFields = ['picture', 'fullname', 'birthday'];
+
+	optionalFields.forEach((field) => {
+		if (data[field]) baseData[field] = data[field];
+	});
+
+	if (data.gdpr_consent === true) baseData.gdpr_consent = 1;
+	if (data.acceptTos === true) baseData.acceptTos = 1;
+
+	return baseData;
+}
+
+async function ensureUniqueUsername(userData, User) {
+	const renamed = await User.uniqueUsername(userData);
+	if (renamed) {
+		userData.username = renamed;
+		userData.userslug = slugify(renamed);
+		userData._usernameChanged = true;
+	}
+	return userData;
+}
+
+async function applyPluginFilters(userData, data) {
+	const results = await plugins.hooks.fire('filter:user.create', { user: userData, data });
+	return results.user;
+}
+
+async function assignUid() {
+	const uid = await db.incrObjectField('global', 'nextUid');
+	const isFirstUser = uid === 1;
+	return { uid, isFirstUser };
+}
+
+async function saveUserToDb(userData, timestamp) {
+	await db.setObject(`user:${userData.uid}`, userData);
+
+	const bulkAdd = [
+		['username:uid', userData.uid, userData.username],
+		[`user:${userData.uid}:usernames`, timestamp, `${userData.username}:${timestamp}`],
+		['username:sorted', 0, `${userData.username.toLowerCase()}:${userData.uid}`],
+		['userslug:uid', userData.uid, userData.userslug],
+		['users:joindate', timestamp, userData.uid],
+		['users:online', timestamp, userData.uid],
+		['users:postcount', 0, userData.uid],
+		['users:reputation', 0, userData.uid],
+	];
+
+	if (userData.fullname) {
+		bulkAdd.push(['fullname:sorted', 0, `${userData.fullname.toLowerCase()}:${userData.uid}`]);
+	}
+
+	await db.sortedSetAddBulk(bulkAdd);
+}
+
+async function runPostCreationTasks({ userData, data, isFirstUser, User }) {
+	await Promise.all([
+		db.incrObjectField('global', 'userCount'),
+		analytics.increment('registrations'),
+		groups.join(['registered-users', 'unverified-users'], userData.uid),
+		User.notifications.sendWelcomeNotification(userData.uid),
+		storePassword(userData.uid, data.password, User),
+		User.updateDigestSetting(userData.uid, meta.config.dailyDigestFreq),
+	]);
+
+	if (data.email && isFirstUser) {
+		await User.setUserField(userData.uid, 'email', data.email);
+		await User.email.confirmByUid(userData.uid);
+	}
+
+	if (data.email && userData.uid > 1) {
+		await User.email.sendValidationEmail(userData.uid, {
+			email: data.email,
+			template: 'welcome',
+			subject: `[[email:welcome-to, ${meta.config.title || meta.config.browserTitle || 'NodeBB'}]]`,
+		}).catch(err =>
+			winston.error(`[user.create] Validation email failed to send\n[emailer.send] ${err.stack}`));
+	}
+
+	if (userData._usernameChanged) {
+		await User.notifications.sendNameChangeNotification(userData.uid, userData.username);
+	}
+}
+
+async function storePassword(uid, password, User) {
+	if (!password) return;
+	const hash = await User.hashPassword(password);
+	await Promise.all([
+		User.setUserFields(uid, {
+			password: hash,
+			'password:shaWrapped': 1,
+		}),
+		User.reset.updateExpiry(uid),
+	]);
+}
+
+/* ---------- EXPORTS ---------- */
+
 module.exports = function (User) {
+
 	User.create = async function (data) {
 		data.username = data.username.trim();
 		data.userslug = slugify(data.username);
@@ -26,114 +143,32 @@ module.exports = function (User) {
 			await lock(data.email, '[[error:email-taken]]');
 		}
 
+		let uid;
 		try {
-			return await create(data);
+			uid = await create(data, User);
 		} finally {
 			await db.deleteObjectFields('locks', [data.username, data.email]);
 		}
+
+		return uid;
 	};
 
-	async function lock(value, error) {
-		const count = await db.incrObjectField('locks', value);
-		if (count > 1) {
-			throw new Error(error);
-		}
-	}
-
-	async function create(data) {
+	//THE FOLLOWING CREATE function was WRITTEN WITH THE HELP OF CHATGPT TO REDUCE THE cyclomatic complexity count 11
+	async function create(data, User) {
+		console.log('This is BEFORE my refactored code!!!');
 		const timestamp = data.timestamp || Date.now();
-
-		let userData = {
-			username: data.username,
-			userslug: data.userslug,
-			joindate: timestamp,
-			lastonline: timestamp,
-			status: 'online',
-		};
-		['picture', 'fullname', 'birthday'].forEach((field) => {
-			if (data[field]) {
-				userData[field] = data[field];
-			}
-		});
-		if (data.gdpr_consent === true) {
-			userData.gdpr_consent = 1;
-		}
-		if (data.acceptTos === true) {
-			userData.acceptTos = 1;
-		}
-
-		const renamedUsername = await User.uniqueUsername(userData);
-		const userNameChanged = !!renamedUsername;
-		if (userNameChanged) {
-			userData.username = renamedUsername;
-			userData.userslug = slugify(renamedUsername);
-		}
-
-		const results = await plugins.hooks.fire('filter:user.create', { user: userData, data: data });
-		userData = results.user;
-
-		const uid = await db.incrObjectField('global', 'nextUid');
-		const isFirstUser = uid === 1;
+		let userData = buildUserData(data, timestamp);
+		userData = await ensureUniqueUsername(userData, User);
+		userData = await applyPluginFilters(userData, data);
+		const { uid, isFirstUser } = await assignUid();
+		console.log('This is IN THE MIDDLE OF my refactored code!!!');
 		userData.uid = uid;
-
-		await db.setObject(`user:${uid}`, userData);
-
-		const bulkAdd = [
-			['username:uid', userData.uid, userData.username],
-			[`user:${userData.uid}:usernames`, timestamp, `${userData.username}:${timestamp}`],
-			['username:sorted', 0, `${userData.username.toLowerCase()}:${userData.uid}`],
-			['userslug:uid', userData.uid, userData.userslug],
-			['users:joindate', timestamp, userData.uid],
-			['users:online', timestamp, userData.uid],
-			['users:postcount', 0, userData.uid],
-			['users:reputation', 0, userData.uid],
-		];
-
-		if (userData.fullname) {
-			bulkAdd.push(['fullname:sorted', 0, `${userData.fullname.toLowerCase()}:${userData.uid}`]);
-		}
-
-		await Promise.all([
-			db.incrObjectField('global', 'userCount'),
-			analytics.increment('registrations'),
-			db.sortedSetAddBulk(bulkAdd),
-			groups.join(['registered-users', 'unverified-users'], userData.uid),
-			User.notifications.sendWelcomeNotification(userData.uid),
-			storePassword(userData.uid, data.password),
-			User.updateDigestSetting(userData.uid, meta.config.dailyDigestFreq),
-		]);
-
-		if (data.email && isFirstUser) {
-			await User.setUserField(uid, 'email', data.email);
-			await User.email.confirmByUid(userData.uid);
-		}
-
-		if (data.email && userData.uid > 1) {
-			await User.email.sendValidationEmail(userData.uid, {
-				email: data.email,
-				template: 'welcome',
-				subject: `[[email:welcome-to, ${meta.config.title || meta.config.browserTitle || 'NodeBB'}]]`,
-			}).catch(err => winston.error(`[user.create] Validation email failed to send\n[emailer.send] ${err.stack}`));
-		}
-		if (userNameChanged) {
-			await User.notifications.sendNameChangeNotification(userData.uid, userData.username);
-		}
-		plugins.hooks.fire('action:user.create', { user: userData, data: data });
+		await saveUserToDb(userData, timestamp);
+		await runPostCreationTasks({ userData, data, isFirstUser, User });
+		plugins.hooks.fire('action:user.create', { user: userData, data });
+		console.log('This is AFTER my refactored code!!!');
 		return userData.uid;
-	}
 
-	async function storePassword(uid, password) {
-		if (!password) {
-			return;
-		}
-		const hash = await User.hashPassword(password);
-		await Promise.all([
-			User.setUserFields(uid, {
-				password: hash,
-				'password:shaWrapped': 1,
-			}),
-			User.reset.updateExpiry(uid),
-		]);
 	}
 
 	User.isDataValid = async function (userData) {
@@ -151,16 +186,13 @@ module.exports = function (User) {
 
 		if (userData.email) {
 			const available = await User.email.available(userData.email);
-			if (!available) {
-				throw new Error('[[error:email-taken]]');
-			}
+			if (!available) throw new Error('[[error:email-taken]]');
 		}
 	};
 
 	User.isPasswordValid = function (password, minStrength) {
 		minStrength = (minStrength || minStrength === 0) ? minStrength : meta.config.minimumPasswordStrength;
 
-		// Sanity checks: Checks if defined and is string
 		if (!password || !utils.isPasswordValid(password)) {
 			throw new Error('[[error:invalid-password]]');
 		}
@@ -192,4 +224,5 @@ module.exports = function (User) {
 			numTries += 1;
 		}
 	};
+
 };
